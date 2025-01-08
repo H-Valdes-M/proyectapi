2ed3b4a05093da4d2ab26bf31534b7942df69f2c
H-Valdes-M <hugovm1.16@gmail.com>
2025-01-08 01:37:20 -0300

    Edicion al Restock

---
+++ database/migrations/2025_01_07_125237_create_restock_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {if (!Schema::hasTable('restock')) {
+        Schema::create('restock', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('producto');
+            $table->unsignedBigInteger('usuario');
+            $table->date('fecha');
+            $table->integer('cant_unidades');
+            $table->string('coment')->nullable();  // Comentarios (opcional)
+            $table->string('doc')->nullable();     // Documento relacionado (opcional)
+            $table->timestamps();
+            $table->softDeletes(); // Borrado lógico
+
+            $table->foreign('producto')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('usuario')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restock');
+    }
+};
+
